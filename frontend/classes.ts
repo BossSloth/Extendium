d5d9c52c32d9acc55ab47cc8fb9d593831d70217
Boss Sloth <tddebart@gmail.com>
2026-03-21 21:13:34 +0100

    fix: toolbar context menu not being styled by themes

--- frontend/classes.ts
+++ frontend/classes.ts
@@ -4,3 +4,4 @@
 import { ClassModule, findClassModule } from '@steambrew/client';
 
 export const settingsClasses = findClassModule(m => m.SettingsTitleBar && m.SettingsDialogButton) as ClassModule;
+export const contextMenuClasses = findClassModule(m => m.contextMenuContents && m.ContextMenuPosition) as ClassModule;
--- frontend/components/Styles.tsx
+++ frontend/components/Styles.tsx
@@ -1,8 +1,17 @@
 import { constSysfsExpr } from '@steambrew/client';
+import { contextMenuClasses } from 'classes';
 import React from 'react';
 
-export const extendiumStyles = constSysfsExpr('extendium.css', { basePath: '../../public' }).content;
+export const extendiumStyles = fixInternalStyles(constSysfsExpr('extendium.css', { basePath: '../../public' }).content);
 
 export function Styles(): React.JSX.Element {
   return <style>{extendiumStyles}</style>;
+}
+
+function fixInternalStyles(styleStr: string): string {
+  for (const [key, value] of Object.entries(contextMenuClasses)) {
+    styleStr = styleStr.replaceAll(new RegExp(`\\.__${key}\\b`, 'g'), `.${value}`);
+  }
+
+  return styleStr;
 }
